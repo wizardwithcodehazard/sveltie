fb07de54125fcaf443a8abd938f9610f7b69dccc
sahil rane <sahilrane249@gmail.com>
2024-08-25 22:51:45 +0530

    added tailwind

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+import Navbar from '$lib/Navbar.svelte'
+import Footer from '$lib/Footer.svelte'
+import "../app.css"
+</script>
+<!-- svelte-ignore component-name-lowercase -->
+<Navbar/>
+<slot />
+<Footer />
