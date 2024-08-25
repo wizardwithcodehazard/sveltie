d2b3a7255296637909cf08b8ff8d2e5bf9d6e81e
sahil rane <sahilrane249@gmail.com>
2024-08-25 21:05:18 +0530

    Initial commit

---
+++ src/routes/blog/[slug]/+page.server.js
@@ -0,0 +1,7 @@
+export async function load ({params}) {
+    return {
+        content : `hello ${params.slug}`
+    }
+
+
+}
---
+++ src/routes/blog/list/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+    import table from "$lib/table.svelte";
+</script>
+
+<h1>List of Blogs</h1>
+<table/>
